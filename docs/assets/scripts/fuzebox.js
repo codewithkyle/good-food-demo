f01b0c90cf753a88dc4c13c0bd2a9d8d43aaf286
Kyle Andrews <kylea@page.works>
2019-06-18 08:05:36 -0400

    Added initial hero html

--- docs/assets/scripts/fuzebox.js
+++ docs/assets/scripts/fuzebox.js
@@ -3,12 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var Fuzebox = (function () {
     function Fuzebox() {
         console.log('Fuzebox has stared');
+        this._globalScriptRequestIndex = 0;
         this.init();
     }
     Fuzebox.prototype.init = function () {
         var _this = this;
         if ('reqeustIdleCallback' in window) {
-            window.requestIdleCallback(function () { _this.loadGlobal(); });
+            window.requestIdleCallback(function () {
+                _this.loadGlobal();
+            });
         }
         else {
             this.loadGlobal();
@@ -31,9 +34,7 @@ var Fuzebox = (function () {
                 }
             }
             if (response.scripts.length) {
-                for (var i = 0; i < response.scripts.length; i++) {
-                    _this.loadScript(response.scripts[i]);
-                }
+                _this.loadGlobalScripts(response.scripts);
             }
         })
             .catch(function (e) {
@@ -59,23 +60,39 @@ var Fuzebox = (function () {
             console.error("Failed to load stylesheet " + filename, e);
         });
     };
-    Fuzebox.prototype.loadScript = function (filename) {
-        fetch("" + window.location.origin + window.location.pathname + "assets/scripts/" + filename, {
-            headers: new Headers({
-                'X-Requested-With': 'XMLHttpRequest'
-            }),
-            credentials: 'include'
+    Fuzebox.prototype.loadGlobalScripts = function (scripts) {
+        var _this = this;
+        new Promise(function (resolve) {
+            var _loop_1 = function (i) {
+                var filename = scripts[i];
+                _this._globalScriptRequestIndex++;
+                var requestIndex = _this._globalScriptRequestIndex;
+                fetch("" + window.location.origin + window.location.pathname + "assets/scripts/" + filename, {
+                    headers: new Headers({
+                        'X-Requested-With': 'XMLHttpRequest'
+                    }),
+                    credentials: 'include'
+                })
+                    .then(function (request) { return request.text(); })
+                    .then(function (response) {
+                    var newScript = document.createElement('script');
+                    newScript.innerHTML = response;
+                    newScript.setAttribute('type', 'text/javascript');
+                    newScript.setAttribute('src', "" + window.location.origin + window.location.pathname + "assets/scripts/" + filename);
+                    document.body.appendChild(newScript);
+                    if (_this._globalScriptRequestIndex === requestIndex) {
+                        resolve();
+                    }
+                })
+                    .catch(function (e) {
+                    console.error("Failed to load stylesheet " + filename, e);
+                });
+            };
+            for (var i = 0; i < scripts.length; i++) {
+                _loop_1(i);
+            }
         })
-            .then(function (request) { return request.text(); })
-            .then(function (response) {
-            var newScript = document.createElement('script');
-            newScript.innerHTML = response;
-            newScript.setAttribute('type', 'text/javascript');
-            newScript.setAttribute('src', "" + window.location.origin + window.location.pathname + "assets/scripts/" + filename);
-            document.body.appendChild(newScript);
-        })
-            .catch(function (e) {
-            console.error("Failed to load stylesheet " + filename, e);
+            .then(function () {
         });
     };
     return Fuzebox;
--- utils/scripts/Fuzebox.ts
+++ utils/scripts/Fuzebox.ts
@@ -1,13 +1,19 @@
 export class Fuzebox{
+
+    private _globalScriptRequestIndex:number;
+
     constructor(){
         console.log('Fuzebox has stared');
+        this._globalScriptRequestIndex = 0;
         this.init();
     }
 
     private init():void{
         if('reqeustIdleCallback' in window){
             // @ts-ignore
-            window.requestIdleCallback(()=>{ this.loadGlobal(); });
+            window.requestIdleCallback(()=>{ 
+                this.loadGlobal();
+             });
         }else{
             this.loadGlobal();
         }
@@ -30,9 +36,7 @@ export class Fuzebox{
             }
 
             if(response.scripts.length){
-                for(let i = 0; i < response.scripts.length; i++){
-                    this.loadScript(response.scripts[i]);
-                }
+                this.loadGlobalScripts(response.scripts);
             }
         })
         .catch(e => {
@@ -60,23 +64,39 @@ export class Fuzebox{
         });
     }
 
-    private loadScript(filename:string):void{
-        fetch(`${ window.location.origin }${ window.location.pathname }assets/scripts/${ filename }`,{
-            headers: new Headers({
-                'X-Requested-With': 'XMLHttpRequest'
-            }),
-            credentials: 'include'
+    private loadGlobalScripts(scripts:Array<string>):void{
+        new Promise((resolve)=>{
+            for(let i = 0; i < scripts.length; i++){
+                const filename = scripts[i];
+
+                this._globalScriptRequestIndex++;
+                const requestIndex = this._globalScriptRequestIndex;
+
+                fetch(`${ window.location.origin }${ window.location.pathname }assets/scripts/${ filename }`,{
+                    headers: new Headers({
+                        'X-Requested-With': 'XMLHttpRequest'
+                    }),
+                    credentials: 'include'
+                })
+                .then(request => request.text())
+                .then(response => {
+                    const newScript = document.createElement('script');
+                    newScript.innerHTML = response;
+                    newScript.setAttribute('type', 'text/javascript');
+                    newScript.setAttribute('src', `${ window.location.origin }${ window.location.pathname }assets/scripts/${ filename }`);
+                    document.body.appendChild(newScript);
+
+                    if(this._globalScriptRequestIndex === requestIndex){
+                        resolve();
+                    }
+                })
+                .catch(e => {
+                    console.error(`Failed to load stylesheet ${ filename }`, e);
+                });
+            }
         })
-        .then(request => request.text())
-        .then(response => {
-            const newScript = document.createElement('script');
-            newScript.innerHTML = response;
-            newScript.setAttribute('type', 'text/javascript');
-            newScript.setAttribute('src', `${ window.location.origin }${ window.location.pathname }assets/scripts/${ filename }`);
-            document.body.appendChild(newScript);
-        })
-        .catch(e => {
-            console.error(`Failed to load stylesheet ${ filename }`, e);
+        .then(()=>{
+            
         });
     }
 }
